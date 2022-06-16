88d8b60a932c5ab614ccc82432d25601140f996b
Kevin Phillips <thefriendlycoder@gmail.com>
2022-06-16 18:58:29 -0300

    added init subcommand

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,43 @@
+use simple_error::SimpleError;
 use std::{error::Error, fmt::Debug};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-/// Sample command line parser
+/// App for managing files on a OneDrive service
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    // Character string input
-    #[clap(short, long)]
-    name: String,
+    #[clap(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    /// Initialize and authenticate the app
+    Init {
+        #[clap(short, long)]
+        /// Can we intercept authentication requests from the browser?
+        browser: bool,
+    },
+}
+
+fn init(browser: bool) -> Result<(), SimpleError> {
+    if browser {
+        return Err(SimpleError::new("Feature not supported"));
+    }
+    Ok(())
 }
 
 pub fn run() -> MyResult<()> {
-    let config = Args::parse();
-    dbg!(config);
+    let args = Args::parse();
+    match args.cmd {
+        SubCommand::Init { browser } => {
+            println!("Interactive? {}", browser);
+            init(browser)?;
+        }
+    }
+    dbg!(args);
     Ok(())
 }
--- tests/app.rs
+++ tests/app.rs
@@ -5,17 +5,24 @@ use std::error::Error;
 type TestResult = Result<(), Box<dyn Error>>;
 
 #[test]
-fn hello_world() {
-    let value = true;
-    assert!(value == true);
-}
-
-#[test]
 #[should_panic]
 fn test_panic_condition() {
     panic!("Darn it");
 }
 
+#[test]
+fn init_help() -> TestResult {
+    for flag in &["-h", "--help"] {
+        Command::cargo_bin("friendly_uploader")?
+            .arg("init")
+            .arg(flag)
+            .assert()
+            .stdout(predicate::str::contains(
+                "Initialize and authenticate the app",
+            ));
+    }
+    Ok(())
+}
 #[test]
 fn usage() -> TestResult {
     for flag in &["-h", "--help"] {
