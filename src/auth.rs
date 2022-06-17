a7df237bba1a16c8ef0c6352e907d5cb6b905c60
Kevin Phillips <thefriendlycoder@gmail.com>
2022-06-17 19:23:52 -0300

    added more doc strings to project

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,32 @@
-pub fn get_url() -> String {
+use simple_error::SimpleError;
+use std::error::Error;
+use url::Url;
+use urlencoding::encode;
+
+/// Gets a formatted URL that can be pasted into a web browser to request access
+/// to the currently logged in OneDrive users profile for our app
+pub fn get_auth_url() -> String {
     let redirect_uri = "http://localhost:8080/";
     let client_id = "f9b7e56c-0d02-4ba4-b1ee-24a98f591be4";
-    let scope = "onedrive.readwrite offline_access";
+    let scope = encode("onedrive.readwrite offline_access");
     format!("https://login.live.com/oauth20_authorize.srf?client_id={}&scope={}&response_type=code&redirect_uri={}", client_id, scope, redirect_uri)
+}
+
+/// Parses a short lived authentication token from a URL which is generated
+/// from the OneDrive authentication request initiatied by the URL generated
+/// by the get_auth_url() helper method
+///
+/// # Arguments
+///
+/// * `url` - Response URL produced by the OneDrive authentication process
+///           Is expected to have a short lived authentication token encoded
+///           in a query parameter named "code"
+pub fn parse_token(url: &str) -> Result<Box<str>, Box<dyn Error>> {
+    let url_data = Url::parse(url)?;
+    for pair in url_data.query_pairs() {
+        if pair.0 == "code" {
+            return Ok(Box::from(pair.1));
+        }
+    }
+    Err(SimpleError::new("URL did not contain authentication token").into())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use crate::auth::get_url;
+//! Command line tool for managing objects stored in a OneDrive service
+use crate::auth::{get_auth_url, parse_token};
 use simple_error::SimpleError;
+use std::io::{stdin, stdout, Write};
 use std::{error::Error, fmt::Debug};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -25,20 +27,39 @@ enum SubCommand {
     },
 }
 
-fn init(browser: bool) -> Result<(), SimpleError> {
+/// Entry point function for the "init" subcommand
+///
+/// # Arguments
+///
+/// * `browser` - True if the user wants the browser to be automatically
+///               launched by our app, and have the response from the
+///               authentication request automatically intercepted
+fn init_cmd(browser: bool) -> MyResult<()> {
     if browser {
-        return Err(SimpleError::new("Feature not supported"));
+        return Err(SimpleError::new("Feature not supported").into());
     }
-    println!("{}", get_url());
+    println!("Open this URL in your browser: {}", get_auth_url());
+
+    let mut response_url = String::new();
+    print!("Paste the response URL here: ");
+    stdout().flush()?;
+    stdin().read_line(&mut response_url)?;
+
+    let token = parse_token(&response_url)?;
+
+    // TODO: store token in a config file in the users home folder
+    //       make it yaml format and set the mode to 600
+    println!("Token is {}", token);
     Ok(())
 }
 
+/// Entrypoint function for our command line interface
 pub fn run() -> MyResult<()> {
     let args = Args::parse();
     match args.cmd {
         SubCommand::Init { browser } => {
             println!("Interactive? {}", browser);
-            init(browser)?;
+            init_cmd(browser)?;
         }
     }
     dbg!(args);
