1cfdf8606378f7971d077515ba9637ebffd767fb
Kevin Phillips <TheFriendlyCoder@users.noreply.github.com>
2022-06-18 09:56:04 -0300

    Added code for managing auth tokens (#1)
    * Basic authentication token logic
    * added more doc strings to project
    * added basic ls command
    * fixed CI check

---
+++ src/auth.rs
@@ -0,0 +1,98 @@
+//! Primitves for managing OneDrive authentication tokens
+use futures::executor;
+use reqwest;
+use serde::Deserialize;
+use simple_error::SimpleError;
+use std::{collections::HashMap, error::Error};
+use url::Url;
+use urlencoding::encode;
+
+/// URL where the browser will be redirected to after the user approves
+/// access to their OneDrive account for our app
+const REDIRECT_URI: &str = "http://localhost:8080/";
+/// GUID that uniquely identifies our application to OneDrive
+const CLIENT_ID: &str = "f9b7e56c-0d02-4ba4-b1ee-24a98f591be4";
+
+/// Gets a formatted URL that can be pasted into a web browser to request access
+/// to the currently logged in OneDrive users profile for our app
+pub fn get_auth_url() -> String {
+    let scope = encode("onedrive.readwrite offline_access");
+    format!("https://login.live.com/oauth20_authorize.srf?client_id={}&scope={}&response_type=code&redirect_uri={}", CLIENT_ID, scope, REDIRECT_URI)
+}
+
+/// Parses a short lived authentication token from a URL which is generated
+/// from the OneDrive authentication request initiatied by the URL generated
+/// by the get_auth_url() helper method
+///
+/// # Arguments
+///
+/// * `url` - Response URL produced by the OneDrive authentication process
+///           Is expected to have a short lived authentication token encoded
+///           in a query parameter named "code"
+pub fn parse_token(url: &str) -> Result<Box<str>, Box<dyn Error>> {
+    let url_data = Url::parse(url)?;
+    for pair in url_data.query_pairs() {
+        if pair.0 == "code" {
+            return Ok(Box::from(pair.1));
+        }
+    }
+    Err(SimpleError::new("URL did not contain authentication token").into())
+}
+
+#[derive(Deserialize, Debug)]
+/// Parsed JSON response data describing the authentication parameters for
+/// a OneDrive connection
+pub struct Authdata {
+    pub token_type: String,
+    pub expires_in: u32,
+    pub scope: String,
+    pub access_token: String,
+    pub refresh_token: String,
+    pub user_id: String,
+}
+
+/// Returns authorization data from OneDrive for the current application
+///
+/// # Arguments
+///
+/// * `client_code` - temporary authentication code provided by OneDrive after
+///                   the user has accepted the authentication request for
+///                   the application
+pub fn get_auth_data(client_code: &str) -> Result<Authdata, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+
+    let url = "https://login.live.com/oauth20_token.srf";
+    let mut params = HashMap::new();
+    params.insert("client_id", CLIENT_ID);
+    params.insert("redirect_uri", REDIRECT_URI);
+    params.insert("code", client_code);
+    params.insert("grant_type", "authorization_code");
+
+    let response = executor::block_on(client.post(url).form(&params).send())?;
+    let data: Authdata = executor::block_on(response.json::<Authdata>())?;
+    Ok(data)
+}
+
+/// Returns new authentication parameters for OneDrive renewing the auth token
+/// expiration in the process
+///
+/// # Arguments
+///
+/// * `refresh_token` - temporary authentication token loaded previously which allows
+///                     us to request a new, longer term use auth token from OneDrive
+///                     Returned auth data will include a new refresh token for use
+///                     in subsequent calls
+pub fn refresh_auth_data(refresh_token: &str) -> Result<Authdata, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+
+    let url = "https://login.live.com/oauth20_token.srf";
+
+    let mut params = HashMap::new();
+    params.insert("client_id", CLIENT_ID);
+    params.insert("redirect_uri", REDIRECT_URI);
+    params.insert("refresh_token", refresh_token);
+    params.insert("grant_type", "refresh_token");
+    let response = executor::block_on(client.post(url).form(&params).send())?;
+    let data: Authdata = executor::block_on(response.json::<Authdata>())?;
+    Ok(data)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+//! Command line tool for managing objects stored in a OneDrive service
+use crate::auth::{get_auth_data, get_auth_url, parse_token};
+use serde::{Deserialize, Serialize};
 use simple_error::SimpleError;
+use std::fs::File;
+use std::io::{stdin, stdout, Read, Write};
+use std::os::unix::fs::PermissionsExt;
 use std::{error::Error, fmt::Debug};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 use clap::{Parser, Subcommand};
+pub mod auth;
 
 /// App for managing files on a OneDrive service
 #[derive(Parser, Debug)]
@@ -21,21 +28,78 @@ enum SubCommand {
         /// Can we intercept authentication requests from the browser?
         browser: bool,
     },
+    /// List contents of root OneDrive folder
+    Ls,
 }
 
-fn init(browser: bool) -> Result<(), SimpleError> {
+#[derive(Serialize, Deserialize)]
+pub struct Configuration {
+    /// Primary authentication token used to connect to OneDrive
+    /// If this token expires we need to use the refresh_token
+    /// to renew it
+    pub auth_token: String,
+    /// Secondary authentication token used to renew the lifetime
+    /// of the primary authentication token
+    pub refresh_token: String,
+}
+
+/// Entry point function for the "init" subcommand
+///
+/// # Arguments
+///
+/// * `browser` - True if the user wants the browser to be automatically
+///               launched by our app, and have the response from the
+///               authentication request automatically intercepted
+fn init_cmd(browser: bool) -> MyResult<()> {
     if browser {
-        return Err(SimpleError::new("Feature not supported"));
+        return Err(SimpleError::new("Feature not supported").into());
     }
+    println!("Open this URL in your browser: {}", get_auth_url());
+
+    let mut response_url = String::new();
+    print!("Paste the response URL here: ");
+    stdout().flush()?;
+    stdin().read_line(&mut response_url)?;
+
+    let token = parse_token(&response_url)?;
+
+    let auth = get_auth_data(&token)?;
+
+    let config = Configuration {
+        auth_token: auth.access_token,
+        refresh_token: auth.refresh_token,
+    };
+
+    let s = serde_yaml::to_string(&config)?;
+    let mut file = File::create("config.yml")?;
+    let mut perms = file.metadata()?.permissions();
+    perms.set_mode(0o600);
+    file.set_permissions(perms)?;
+    file.write_all(s.as_bytes())?;
+
     Ok(())
 }
 
+/// Entrypoint method for the 'ls' subcommand
+/// Shows a directory listing of the root OneDrive folder
+fn ls_cmd() -> MyResult<()> {
+    let mut file = File::open("config.yml")?;
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+    let config: Configuration = serde_yaml::from_str(&s)?;
+    println!("Token is {}", config.auth_token);
+    Ok(())
+}
+
+/// Entrypoint function for our command line interface
 pub fn run() -> MyResult<()> {
     let args = Args::parse();
     match args.cmd {
         SubCommand::Init { browser } => {
-            println!("Interactive? {}", browser);
-            init(browser)?;
+            init_cmd(browser)?;
+        }
+        SubCommand::Ls => {
+            ls_cmd()?;
         }
     }
     dbg!(args);
